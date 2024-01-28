83a95b68ecc0f945f1debbe2fdc79a8a249ddbe7
Junior Rabelo <rabelojunior105@gmail.com>
2024-01-28 18:08:19 -0300

    reduzido o codigo

--- app.js
+++ app.js
@@ -3,53 +3,74 @@ const app = express();
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
-var n1 = 0;
-var n2 = 0;
-var n3 = 0;
-var n4 = 0;
-var n5 = 0;
 
-var bancoTemp = [	
+
+var kanjis = [	
 	//Kanjis N5
-	["日","5"],
-	//Kanjis N4
-	["会","4"],
-	//Kanjis N3
-	["政","3"],
-	//Kanjis N2
-	["党","2"],
-	//Kanjis N1
-	["氏","1"]
+        {
+            kanji:"日",
+            level:"5"
+        },
+        {
+            kanji:"会",
+            level:"4"
+        },
+        {
+            kanji:"政",
+            level:"3"
+        },
+        {
+            kanji:"党",
+            level:"2"
+        },
+        {
+            kanji:"氏",
+            level:"1"
+        }
+];
+
+const characteres = [
+    {
+        regex: /[?]/g,
+        replace: '2'
+    },
+    {
+        regex: /[a]/g,
+        replace: '3'
+    }
 ];
 
 
 
 
 app.get("/", (req, res, next) => {
+    var level = 0
 
-
-  characteres = [
-        {
-            regex: /[?]/g,
-            replace: '2'
-        },
-        {
-            regex: /[a]/g,
-            replace: '3'
-        }
-    ];
-    
-
-    texto = "teste de exclusão de ? e a 会党";
+    texto = "日 teste de exclusão de ? e a 日 党";
 
     for (const e of characteres) {
         texto = texto.replace(e.regex, e.replace)
     }
-    //res.json({ texto })
-    res.json({ texto })
-    txtArray = texto.split(''); //transforma string em array
 
-    txtArray.forEach((caracter)=>{
+
+    for(const charactere of kanjis){
+        const matchRegex = texto.match(charactere.kanji)
+            if(matchRegex){
+                level++
+            }
+    }
+
+    res.json({
+        texto,
+        level
+    })
+
+});
+
+
+
+//codigo do wesley 
+/*     txtArray.forEach((caracter)=>{
         for(var lin = 0 ; lin < bancoTemp.length; lin++){
             for(var col = 0 ; col < 2 ; col++){
 
@@ -69,14 +90,14 @@ app.get("/", (req, res, next) => {
         }
         
     });
-
-    console.log(n1);
+ */
+/*     console.log(n1);
     console.log(n2);
     console.log(n3);
     console.log(n4);
     console.log(n5);
-    console.log(aux); 
-});
+    console.log(aux);  */
+
 
 
 
